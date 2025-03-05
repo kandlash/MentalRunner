3da44875058ae6a65bf97408634668b60f6e2336
Konstantin Lashkulov <konstantin.laschkulov@yandex.com>
2025-03-05 23:12:50 +0300

    katana attack and enemys

---
+++ enemy.gd
@@ -0,0 +1,4 @@
+extends Area3D
+
+func take_damage():
+	queue_free()
--- scripts/katana_arm.gd
+++ scripts/katana_arm.gd
@@ -1,7 +1,21 @@
 extends Node3D
 
+var can_attack = true
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("attack"):
+	if Input.is_action_just_pressed("attack") and can_attack:
 		$"../../../AnimationPlayer".play("katana_attack_1")
 		$"../slash".play_animation()
+		$"../../../katana_attack".play()
+		$"../attack_area".monitoring = true
+		can_attack = false
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "katana_attack_1":
+		$"../attack_area".monitoring = false
+		can_attack = true
+
+
+func _on_attack_area_area_entered(area: Area3D) -> void:
+	if area.is_in_group("enemy"):
+		area.take_damage()
